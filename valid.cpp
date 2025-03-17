452636aee1ad3d6e35db1942a3042d587849545f
Vaghaniharshh <hvaghani89@gmail.com>
2025-03-17 10:07:45 +0530

    Add files via upload

---
+++ valid.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Equal(const string& s) {
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]); 
+        } 
+        else {
+
+           
+            if (!st.empty() && 
+                ((st.top() == '(' && s[i] == ')') ||
+                 (st.top() == '{' && s[i] == '}') ||
+                 (st.top() == '[' && s[i] == ']'))) {
+
+                
+                st.pop(); 
+            }
+            else {
+                
+               
+                return false; 
+            }
+        }
+    }
+    
+    
+    return st.empty();
+}
+
+int main() {  
+    string s = "{([])}";
+    if (Equal(s))
+        cout << "true";
+    else
+        cout << "false";
+    return 0;
+}
